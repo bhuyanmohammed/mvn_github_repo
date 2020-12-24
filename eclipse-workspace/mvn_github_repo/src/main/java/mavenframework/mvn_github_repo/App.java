8891b23dc2f042965186a722fba36f706fff6d6e
biplob <bhuyan.shiful1979@gmail.com>
2020-12-24 14:07:20 -0500

    First commit.......

---
+++ eclipse-workspace/mvn_github_repo/src/main/java/mavenframework/mvn_github_repo/App.java
@@ -0,0 +1,13 @@
+package mavenframework.mvn_github_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ eclipse-workspace/mvn_github_repo/src/test/java/testing_a_repo/Test_1.java
@@ -0,0 +1,19 @@
+package testing_a_repo;
+
+
+import org.testng.annotations.Test;
+
+@Test
+public class Test_1 {
+	int a = 10 ;
+	int b = 20;
+	int c= a+b;
+
+	
+
+	
+	
+	
+	
+
+}
